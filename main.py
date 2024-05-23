1b044bdc74f02b3ae8b9d94302098e040b25e5d0
AtirhGael <atirhg@gmail.com>
2024-05-23 23:45:34 +0100

    commited

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-from  sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn import svm 
-from sklearn.metrics import accuracy_score
-import numpy as np
-import pandas as pd
+import pandas as pd 
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
 
-iris  = datasets.load_iris()
 
-x= iris.data
-y = iris.target
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+temp = pd.read_csv("temp.csv", parse_dates=["datetime"],index_col='datetime' )
 
-classes = ['Iris Setosa','Iris Versicolour','Iris Virginica']
 
-model = svm.SVC()
-model.fit(x_train,y_train)
- 
-print(model)
-prediction = model.predict(x_test)
-acc = accuracy_score(y_test,prediction)
+pd.to_datetime(temp.datetime[0])
+# print(temp.info())
 
-print('predictions',prediction)
-print('accuracy',acc)
+# downtiming series
+temp.resample('d')
+print(temp)
+
+
+
+
+
+
+
+
 
 # env\Scripts\activate
