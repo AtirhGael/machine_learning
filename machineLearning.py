ce4ff7150db572a0a397745b8ee5dbaf3413e77c
AtirhGael <atirhg@gmail.com>
2024-06-17 22:40:58 +0100

    comitted

--- machineLearning.py
+++ machineLearning.py
@@ -25,13 +25,24 @@ print(data_pandas.age>20)
 # %%
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+import mglearn
+
+knn = KNeighborsClassifier(n_neighbors=1)
 
 iris_data  = load_iris()
 X_train, X_test,y_train,y_test = train_test_split(iris_data['data'],iris_data['target'],random_state=0)
 # print(y_train.shape)
 iris_dataframe = pd.DataFrame(X_train,columns=iris_data.feature_names)
-print(iris_dataframe)
-grr = pd.scatter_matrix(iris_data,c=y_train, figsize=(15,15),marker='0', hist_kwds={'bin':20},s=60,alpha=8,cmap=mglearn.cm3)
+# print(iris_dataframe)
+grr = pd.plotting.scatter_matrix(iris_dataframe,c=y_train, figsize=(15,15),marker='x', hist_kwds={'bins':20},s=60,alpha=8,cmap=mglearn.cm3)
+# print(grr)
+
+knn.fit(X_train,y_train)
+
 
 # %%
+
+
+
